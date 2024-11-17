b1d46c4a938b3259ee2f6f4abe429c697c0a05d2
Don Le <donleqt@gmail.com>
2024-11-17 10:25:29 +0700

    chore: configure prettier eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import { Container, CssBaseline } from '@mui/material';
 import type { Metadata } from 'next';
-import { Roboto } from '@next/font/google';
+import { Roboto } from 'next/font/google';
 
 import './globals.css';
 
